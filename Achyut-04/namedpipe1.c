2edec36ed967968b8e49573bf9f66d813215744a
Achyut Shekhar Singh <158504706+Achyut-shekhar@users.noreply.github.com>
2025-09-30 11:33:47 +0530

    Update namedpipe1.c
    added to print number of bytes

--- Achyut-04/namedpipe1.c
+++ Achyut-04/namedpipe1.c
@@ -1,32 +1,34 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include<fcntl.h>
-#include<sys/stat.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
 
 int main()
 {
    int fd;
-   
-   char * myfifo="/tmp/myfifo";
-   
-   
+   char *myfifo = "/tmp/myfifo";
+
    mkfifo(myfifo, 0666);
-   
-   char arr1[80],arr2[80];
-   while(1)
+
+   char arr1[80], arr2[80];
+   int bytesWritten, bytesRead;
+
+   while (1)
    {
-      fd=open(myfifo, O_WRONLY);
-      fgets(arr2,80,stdin);
-      write(fd,arr2,strlen(arr2)+1);
+      fd = open(myfifo, O_WRONLY);
+      fgets(arr2, 80, stdin);
+      bytesWritten = write(fd, arr2, strlen(arr2) + 1);
+      printf("Bytes written: %d\n", bytesWritten);
       close(fd);
-      
-      fd=open(myfifo, O_RDONLY);
-      
-      read(fd, arr1, sizeof(arr1));
-      
-      printf("USER 2: %s\n",arr1);
+
+      fd = open(myfifo, O_RDONLY);
+      bytesRead = read(fd, arr1, sizeof(arr1));
+      printf("USER 2: %s", arr1);
+      printf("Bytes read: %d\n", bytesRead);
+      close(fd);
    }
    return 0;
 }
